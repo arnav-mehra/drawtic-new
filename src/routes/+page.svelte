485339591a5944b41d40a098b441e1ff9f06bb43
ArnavMeh <mehraarn000@gmail.com>
2023-12-19 07:40:32 -0500

    big bang

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,64 @@
 <script>
     import { ButtonGroup, Button, GradientButton } from 'flowbite-svelte';
+    import logo from '$lib/assets/logo.png';
+    import { onMount } from 'svelte';
+
+    let welcome_opacity = 0;
+    let start_opacity = 0;
+
+    onMount(() => {
+        const ts = [
+            setTimeout(() => welcome_opacity = 1, 1000),
+            setTimeout(() => welcome_opacity = 0, 3000),
+            setTimeout(() => start_opacity = 1, 6000),
+        ];
+        return () => ts.forEach(clearTimeout);
+    });
 </script>
 
-<div id="home-container">
-    <div id="home-title">
-        Welcome to Gartic Phone
+<style>
+    #container {
+        width: 100vw;
+        height: 100vh;
+        position: fixed;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    
+    #welcome {
+        position: absolute;
+        font-size: 18px;
+        transition: opacity 2.0s;
+    }
+
+    #start {
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        transition: opacity 1.0s;
+    }
+
+    #logo {
+        max-width: 400px;
+        margin-bottom: 30px;
+    }
+</style>
+
+<div id="container">
+    <div id="welcome" style={`opacity: ${welcome_opacity}`}>
+        Welcome to...
     </div>
-    <div>
+    <div id="start" style={`opacity: ${start_opacity}`}>
+        <div id="logo">
+            <img id="logo-img" src={logo} alt="oh no"/>
+        </div>
         <ButtonGroup class="space-x-px">
-            <Button pill color="purple">Join Lobby</Button>
-            <Button pill color="purple">Create Lobby</Button>
+            <Button size="lg" pill color="purple">Join Lobby</Button>
+            <Button size="lg" pill color="purple">Create Lobby</Button>
         </ButtonGroup>
     </div>
 </div>
